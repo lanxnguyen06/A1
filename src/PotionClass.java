4d7581d72ee272c9aff45c42e981aea0566c686d
lanxnguyen06 <lanxnguyen06@gmail.com>
2025-10-01 14:01:18 -0400

    comment

--- src/PotionClass.java
+++ src/PotionClass.java
@@ -3,7 +3,7 @@ public class PotionClass {
     public static void main(String[] args) {
         Potion create = new Potion(); // potion object creates a new potion based on the 3 ingredients the user inputs that gets stored in array list cookingPot
         create.addIngredient(); // i combined selectIngredients with addIngredients
-        if (create.question.equalsIgnoreCase("yes")){
+        if (create.question.equalsIgnoreCase("yes")){ // if user says yes to adding > 3 ingredients program stops bc potion explodes
             System.exit(0); // had to look this up cause i didn't know how to make the program stop if user inputs >3 ingredients
         }
         else{
